5f8d26a7be133cdf4f2feabe2a110980dfcf2e66
Patrick J Louis <lumenfuzz@gmail.com>
2018-04-30 21:24:16 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,13 @@
+function iterativeLog(array) {
+  array.forEach() {
+    console.log(`${index}: ${element}`)
+  }
+}
+
+function iterate(callback) {
+  
+}
+
+function doToArray(array, callback) {
+  
+}
